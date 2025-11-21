3610b06c795def48d21844e21c1a5d403ed568ac
aemsAMS <aemz0583@gmail.com>
2025-11-21 12:59:04 +0330

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,72 +1,46 @@
+// abi.js
+const PRESALE_ADDRESS = "0xa6452a6d7d399dd2b1d2c5e237eabd9c1c00379f";
 
-const CONTRACT_ADDRESS = "0xc2a1754cb038c090783531e3285238076c90e61d";
-const TOKEN_ADDRESS = "0x259115680169276d0e4286acba362460456697c5";
-const ABI = [
-  {
-    "inputs": [],
-    "name": "entryFee",
-    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "gameId",
-    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "owner",
-    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "setupRules",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "joinGame",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [{ "internalType": "uint8", "name": "element", "type": "uint8" }],
-    "name": "play",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "forfeit",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-    "name": "games",
-    "outputs": [
-      { "internalType": "address", "name": "player1", "type": "address" },
-      { "internalType": "address", "name": "player2", "type": "address" },
-      { "internalType": "uint8[3]", "name": "p1Choices", "type": "uint8[3]" },
-      { "internalType": "uint8[3]", "name": "p2Choices", "type": "uint8[3]" },
-      { "internalType": "uint8", "name": "round", "type": "uint8" },
-      { "internalType": "uint8", "name": "p1Wins", "type": "uint8" },
-      { "internalType": "uint8", "name": "p2Wins", "type": "uint8" },
-      { "internalType": "uint256", "name": "lastMoveTime", "type": "uint256" },
-      { "internalType": "bool", "name": "isFinished", "type": "bool" },
-      { "internalType": "address", "name": "winner", "type": "address" }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  }
+// Minimum ABI (functions/events used by the front-end)
+const PRESALE_ABI = [
+  {"inputs":[],"stateMutability":"nonpayable","type":"constructor"},
+  {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"buyer","type":"address"},{"indexed":false,"internalType":"uint256","name":"tokens","type":"uint256"},{"indexed":false,"internalType":"string","name":"currency","type":"string"}],"name":"TokensPurchased","type":"event"},
+  {"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"vestingStart","type":"uint256"}],"name":"PresaleAutoEnded","type":"event"},
+  {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"TokensClaimed","type":"event"},
+  {"inputs":[],"name":"buyWithBNB","outputs":[],"stateMutability":"payable","type":"function"},
+  {"inputs":[{"internalType":"uint256","name":"usdtAmount","type":"uint256"}],"name":"buyWithUSDT","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"getPresaleAllocation","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},
+  {"inputs":[],"name":"getSold","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"getTotalRaisedBNBUSD","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"getTotalRaisedUSDT","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"getVestingStart","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"isPresaleEnded","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},
+  {"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"claimable","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"claimTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"tokensLeft","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[{"internalType":"address","name":"_token","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"withdrawUnsoldPresaleTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"priceFeed","outputs":[{"internalType":"contract IAggregatorV3","name":"","type":"address"}],"stateMutability":"view","type":"function"},
+  {"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"buyers","outputs":[{"internalType":"uint256","name":"purchased","type":"uint256"},{"internalType":"uint256","name":"claimed","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"endPresaleManually","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"destroyContract","outputs":[],"stateMutability":"nonpayable","type":"function"}
 ];
+
+// Minimal ERC20 ABI
+const ERC20_ABI = [
+  "function approve(address spender, uint256 amount) external returns (bool)",
+  "function allowance(address owner, address spender) external view returns (uint256)",
+  "function balanceOf(address account) external view returns (uint256)",
+  "function decimals() external view returns (uint8)",
+  "function transfer(address to, uint256 amount) external returns (bool)",
+  "function transferFrom(address from, address to, uint256 amount) external returns (bool)"
+];
+
+// Chainlink aggregator ABI (latestRoundData)
+const AGG_ABI = [
+  "function latestRoundData() view returns (uint80,int256,uint256,uint256,uint80)"
+];
+
+// USDT address (BSC Mainnet)
+const USDT_ADDRESS = "0x55d398326f99059fF775485246999027B3197955";
