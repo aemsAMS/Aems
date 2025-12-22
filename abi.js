fa6077284fd7479c312bf8ce919b0f4f51bdb430
aemsAMS <aemz0583@gmail.com>
2025-12-22 18:41:44 +0330

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,27 +1,21 @@
 const PRESALE_ADDRESS = "0xa6452a6d7d399dd2b1d2c5e237eabd9c1c00379f";
 
 const PRESALE_ABI = [
-  "function buyWithBNB() payable",
-  "function buyWithUSDT(uint256)",
-  "function claimTokens()",
-  "function getPresaleAllocation() view returns(uint256)",
-  "function getSold() view returns(uint256)",
-  "function tokensLeft() view returns(uint256)",
-  "function buyers(address) view returns(uint256,uint256)",
-  "function claimable(address) view returns(uint256)",
-  "function priceFeed() view returns(address)",
-  "function owner() view returns(address)"
-];
-
-const ERC20_ABI = [
-  "function approve(address,uint256)",
-  "function allowance(address,address) view returns(uint256)",
-  "function balanceOf(address) view returns(uint256)",
-  "function decimals() view returns(uint8)"
-];
-
-const AGG_ABI = [
-  "function latestRoundData() view returns(uint80,int256,uint256,uint256,uint80)"
-];
-
-const USDT_ADDRESS = "0x55d398326f99059fF775485246999027B3197955";
+  {"inputs":[],"stateMutability":"nonpayable","type":"constructor"},
+  {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"buyer","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"BoughtWithUSDT","type":"event"},
+  {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"buyer","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"BoughtWithBNB","type":"event"},
+  {"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"buyWithUSDT","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"buyWithBNB","outputs":[],"stateMutability":"payable","type":"function"},
+  {"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"claimable","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"claim","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"totalSold","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"totalAllocation","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"totalRaisedUSD","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"isActive","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"forceEndPresale","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"withdrawBNB","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"withdrawUSDT","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"withdrawUnsold","outputs":[],"stateMutability":"nonpayable","type":"function"},
+  {"inputs":[],"name":"destroy","outputs":[],"stateMutability":"nonpayable","type":"function"}
+]; 
